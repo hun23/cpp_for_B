1742028ec3af4269ee214fa436b426db4e020e14
felix0942364 <jaewoongcho93@gmail.com>
2023-04-04 17:53:19 +0900

    update base for April fifth cpp studies

---
+++ 0405/JOSEPHUS_JW.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void josephus(int n, int k)
+{
+
+}
+
+int main (void)
+{
+  int T, N, K;
+  cin >> T;
+  while(T--)
+  {
+    cin >> N >> K;
+    josephus(N, K);
+    cout << "result" << endl;
+  }
+
+  return 0;
+}
